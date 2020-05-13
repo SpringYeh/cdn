fb673285692455cebdd309af45e80213845834b5
cinzano <springyeh@163.com>
2020-05-13 14:28:50 +0800

    fix link page

--- apollo/js/main.js
+++ apollo/js/main.js
@@ -51,7 +51,6 @@ $('body').pjax('a[target!=_blank]', {
     inner_loading()
 }).on('pjax:complete', function () {
     init();
-    // AOS.init();
     toggleSidebar();
     tocBot();
     shrinkAuthor();
@@ -104,14 +103,20 @@ function init() {
         additionalMarginBottom: 20
     });
     // * owl.carousel 幻灯片
-    $(function () {
-        $('#owl-banner').owlCarousel({
-            items: 1,
+    var owl = $('#owl-banner');
+    if (owl.length > 0) {
+        owl.owlCarousel({
             loop: true,
+            margin: 0,
+            nav: false,
+            items: 1,
+            dots: false,
             autoplay: true,
-            autoplayHoverPause: true
-        });
-    });
+            autoplayTimeout: 5000,
+            autoplayHoverPause: true,
+            responsiveClass: true
+        })
+    };
     var pointHeight = $('#pjax-container').height();
     $('.points').css('height', pointHeight);
     if ($('#rainbowPoint').attr('data') == 'off') {
@@ -134,8 +139,8 @@ function init() {
         new Valine({
             av: AV,
             el: '#vcomments',
-            emoticon_url: 'https://cloud.panjunwen.com/alu',
-            emoticon_list: ["吐.png", "喷血.png", "狂汗.png", "不说话.png", "汗.png", "坐等.png", "献花.png", "不高兴.png", "中刀.png", "害羞.png", "皱眉.png", "小眼睛.png", "中指.png", "尴尬.png", "瞅你.png", "想一想.png", "中枪.png", "得意.png", "肿包.png", "扇耳光.png", "亲亲.png", "惊喜.png", "脸红.png", "无所谓.png", "便便.png", "愤怒.png", "蜡烛.png", "献黄瓜.png", "内伤.png", "投降.png", "观察.png", "看不见.png", "击掌.png", "抠鼻.png", "邪恶.png", "看热闹.png", "口水.png", "抽烟.png", "锁眉.png", "装大款.png", "吐舌.png", "无奈.png", "长草.png", "赞一个.png", "呲牙.png", "无语.png", "阴暗.png", "不出所料.png", "咽气.png", "期待.png", "高兴.png", "吐血倒地.png", "哭泣.png", "欢呼.png", "黑线.png", "喜极而泣.png", "喷水.png", "深思.png", "鼓掌.png", "暗地观察.png"],
+            emoticon_url: 'https://cdn.jsdelivr.net/gh/SpringYeh/cdn/alu',
+            emoticon_list: ["吐.png", "狂汗.png", "不说话.png", "汗.png", "献花.png", "不高兴.png", "中刀.png", "皱眉.png", "小眼睛.png", "中指.png", "尴尬.png", "瞅你.png", "想一想.png", "中枪.png", "得意.png", "肿包.png", "扇耳光.png", "亲亲.png", "惊喜.png", "脸红.png", "无所谓.png", "便便.png", "愤怒.png", "蜡烛.png", "献黄瓜.png", "内伤.png", "投降.png", "观察.png", "看不见.png", "击掌.png", "抠鼻.png", "邪恶.png", "看热闹.png", "口水.png", "抽烟.png", "锁眉.png", "装大款.png", "吐舌.png", "无奈.png", "长草.png", "赞一个.png", "无语.png", "阴暗.png", "不出所料.png", "咽气.png", "期待.png", "高兴.png", "吐血倒地.png", "哭泣.png", "欢呼.png", "黑线.png", "喷水.png", "深思.png", "鼓掌.png", "暗地观察.png"],
             app_id: 'JJ622U1oUpMeJpbCgps1EwV9-MdYXbMMI',
             app_key: 'FtBjhGaTWOgLi7C8Rtk6UvTK',
             placeholder: '你是我一生只会遇见一次的惊喜 ：）',
